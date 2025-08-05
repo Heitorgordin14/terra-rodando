a4f7163c83bdbc31d05855cabe3127276d539492
Heitorgordin14 <castilho.heitor@escola.pr.gov.br>
2025-08-05 10:11:59 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,11 @@
 function setup() {
-  createCanvas(1920, 1080, WEBGL); //plano 3D
-  terra=loadImage('terra.jpg')
+  createCanvas(1920, 1080, WEBGL); // plano 3D
+  terra = loadImage('terra.jpg');
 }
 
 function draw() {
-  background(o, o, o); //cor de fundo
-  rotateY(millis()/1000); // rotação da terra
-  texture(terra); //textura da terra, imagem.
-  sphere(80, 100); //esfera 3D
-  
-  
+  background(50, 50, 150); // Cor de fundo: um tom de azul escuro para simular o espaço
+  rotateY(millis() / 5000); // Rotação da terra mais lenta
+  texture(terra); // Textura da terra
+  sphere(200); // Esfera 3D maior, com raio de 200
 }
